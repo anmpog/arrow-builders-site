9c788a179a57dd4b7064008f3c7f2d18c6f6d3bf
Anthony Pogliano <poglianoster@gmail.com>
2021-02-01 16:31:59 -0700

    Removed the React Reveal package and implementations from project.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react';
 // import { Link } from 'gatsby';
 import { Link } from 'react-scroll';
-import Fade from 'react-reveal/Fade';
 import { Container, Row, Col } from 'react-bootstrap';
 import Title from '../Title/Title';
 import AboutImg from '../Image/AboutImg';
@@ -30,35 +29,31 @@ const About = () => {
         <Title title='About Us' />
         <Row className='about-wrapper'>
           <Col md={6} sm={12}>
-            <Fade bottom duration={1000} delay={600} distance='30px'>
-              <div className='about-wrapper__image'>
-                <AboutImg alt='profile picture' filename={img} />
-              </div>
-            </Fade>
+            <div className='about-wrapper__image'>
+              <AboutImg alt='profile picture' filename={img} />
+            </div>
           </Col>
           <Col md={6} sm={12}>
-            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance='30px'>
-              <div className='about-wrapper__info'>
-                <p className='about-wrapper__info-text'>
-                  {paragraphOne ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
-                </p>
-                <p className='about-wrapper__info-text'>
-                  {paragraphTwo ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
-                </p>
-                <p className='about-wrapper__info-text'>
-                  {paragraphThree || 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'}
-                </p>
-                {resume && (
-                  <span className='d-flex mt-3'>
-                    <Link to='projects' smooth duration={1000}>
-                      Examples of Our Work
-                    </Link>
-                  </span>
-                )}
-              </div>
-            </Fade>
+            <div className='about-wrapper__info'>
+              <p className='about-wrapper__info-text'>
+                {paragraphOne ||
+                  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+              </p>
+              <p className='about-wrapper__info-text'>
+                {paragraphTwo ||
+                  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+              </p>
+              <p className='about-wrapper__info-text'>
+                {paragraphThree || 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'}
+              </p>
+              {resume && (
+                <span className='d-flex mt-3'>
+                  <Link to='projects' smooth duration={1000}>
+                    Examples of Our Work
+                  </Link>
+                </span>
+              )}
+            </div>
           </Col>
         </Row>
       </Container>
